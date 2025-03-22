5d49630809b82c0fd6e9b76bf3898e17ba9220c6
Seu Nome <bacochina@gmail.com>
2025-03-22 09:13:01 -0300

    Divisão do Pages de Nova Música em componentes modulares

--- src/app/(routes)/musicas/nova/page.tsx
+++ src/app/(routes)/musicas/nova/page.tsx
@@ -55,12 +55,12 @@ export default function NovaMusicaPage() {
   };
 
   if (!componentsLoaded) {
-    return (
-      <div className="container mx-auto px-4 py-8">
+  return (
+    <div className="container mx-auto px-4 py-8">
         <div className="flex justify-center items-center h-64">
           <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-purple-500"></div>
         </div>
-      </div>
+        </div>
     );
   }
 
